31cb7d2b01e6012c93283cd090aca1a1ff15760b
Vishnu Kanhan <kanhanvishnu@gmail.com>
2024-08-21 15:38:29 +0530

    refactor: moved the core logic to morse package
    -Refactored  to include only initialization and input handling.

--- main.go
+++ main.go
@@ -4,156 +4,43 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
+
+	"github.com/Vkanhan/go-morsecode/morse"
 )
 
-// Morse code dictionary
-var morseCodeMap = map[string]string{
-	".-":    "A",
-	"-...":  "B",
-	"-.-.":  "C",
-	"-..":   "D",
-	".":     "E",
-	"..-.":  "F",
-	"--.":   "G",
-	"....":  "H",
-	"..":    "I",
-	".---":  "J",
-	"-.-":   "K",
-	".-..":  "L",
-	"--":    "M",
-	"-.":    "N",
-	"---":   "O",
-	".--.":  "P",
-	"--.-":  "Q",
-	".-.":   "R",
-	"...":   "S",
-	"-":     "T",
-	"..-":   "U",
-	"...-":  "V",
-	".--":   "W",
-	"-..-":  "X",
-	"-.--":  "Y",
-	"--..":  "Z",
-	"-----": "0",
-	".----": "1",
-	"..---": "2",
-	"...--": "3",
-	"....-": "4",
-	".....": "5",
-	"-....": "6",
-	"--...": "7",
-	"---..": "8",
-	"----.": "9",
-}
-
-// Reverse the dictionary for converting English to Morse code
-var englishToMorseMap = reverseMap(morseCodeMap)
-
-// reverseMap function creates a reversed map for english to morse code
-func reverseMap(originalMap map[string]string) map[string]string {
-	reversedMap := make(map[string]string)
-
-	// Swap key-value pairs from the original map to create the reversed map.
-	for key, value := range originalMap {
-		reversedMap[value] = key
-	}
-	return reversedMap
-}
-
-// Function to convert morse code to english
-func morseCodeToEnglish(morseCode string) string {
-	var decodedMessage strings.Builder
-
-	//words are seperated by 3 spaces
-	words := strings.Split(morseCode, "   ")
-
-	for _, word := range words {
-		//letters are seperated by 1 space
-		letters := strings.Split(word, " ")
-
-		//Look up each letter in the morseCodeMap and append the corresponding
-		// translated letter to the decodedMessage if the letter is found in the map.
-		for _, letter := range letters {
-			if translatedLetter, found := morseCodeMap[letter]; found {
-				decodedMessage.WriteString(translatedLetter)
-			} else {
-				// if the decoded message is not recognized
-				decodedMessage.WriteString("?")
-			}
-		}
-		//Add space after each word
-		decodedMessage.WriteString(" ")
-	}
-	return strings.TrimSpace(decodedMessage.String())
-}
-
-// Function to convert English text to Morse code
-func englishToMorse(englishText string) string {
-	var morseCode strings.Builder
-
-	// Convert text to uppercase to match the Morse code map
-	englishText = strings.ToUpper(englishText)
-
-	for _, char := range englishText {
-		switch {
-		case char == ' ':
-			morseCode.WriteString("  ") // Add two spaces between words as there is already one space don't add three spaces.
-		default:
-			if morse, ok := englishToMorseMap[string(char)]; ok {
-				morseCode.WriteString(morse + " ") // Add a space after each letter
-			} else {
-				morseCode.WriteString("?") // If the symbol is not recognized
-			}
-		}
-	}
-
-	return strings.TrimSpace(morseCode.String()) // Remove the trailing space
-}
-
-// Function to handle user input and output
-func handleUserInput() {
-	// Create a new scanner to read user input
+// handleConversion prompts the user for input, performs the conversion, and displays the result
+func handleConversion(prompt string, converter func(string)string) {
+	// Prompt the user for input
+	fmt.Println(prompt)
 	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
 
-	// Prompt the user to choose an option
-	fmt.Println("Choose an option:")
-	fmt.Println("1. Convert Morse code to English")
-	fmt.Println("2. Convert English to Morse code")
-	fmt.Print("Enter 1 or 2: ")
+	// Perform the conversion using the provided converter function 
+	result := converter(input)
+
+	// Display the result
+	fmt.Println("Result: ", result)
+}
+
+func main() {
+	//Display the menu options to the user
+	fmt.Println("Choose an option:\n1. Convert Morse code to English\n2. Convert English to Morse code\nEnter 1 or 2: ")
+
+	// Create a scanner to read user input
+	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	option := scanner.Text()
 
+	// Handle the user's choice
 	switch option {
 	case "1":
-		// Morse code to English
-		fmt.Println("Enter Morse code (use spaces to separate letters and three spaces to separate words):")
-		scanner.Scan()
-		morseCode := scanner.Text()
-
-		// Convert Morse code to English
-		englishText := morseCodeToEnglish(morseCode)
-
-		// Output the result
-		fmt.Println("English Text:", englishText)
-
+		handleConversion("Enter Morse code (use spaces to separate letters and three spaces to separate words):", morse.MorseCodeToEnglish)
 	case "2":
-		// English to Morse code
-		fmt.Println("Enter English text:")
-		scanner.Scan()
-		englishText := scanner.Text()
-
-		// Convert English to Morse code
-		morseCode := englishToMorse(englishText)
-
-		// Output the result
-		fmt.Println("Morse Code:", morseCode)
-
+		handleConversion("Enter English text:", morse.EnglishToMorse)
 	default:
 		fmt.Println("Invalid option")
 	}
 }
 
-func main() {
-	handleUserInput()
-}
+
